9aa2455854e377c9e4cd03399634167deece1a3b
Lucas Yepez <lucasyepez99@gmail.com>
2025-06-16 21:24:22 -0500

    add localhost:8081 to allowed hosts

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser,Group,Permission
 
 # Create your models here.
-
+#
 class User(AbstractUser):
     # Any additional fields here
 
