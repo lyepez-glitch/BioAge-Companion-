6e90bf97352fc206ad1cda53e7386fd81d96c060
Lucas Yepez <lucasyepez99@gmail.com>
2025-09-02 15:24:39 -0500

    fix db

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,20 +126,20 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#     }
-# }
-
 DATABASES = {
-  'default': dj_database_url.config(default=os.environ["DATABASE_URL"])
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
 }
+
+# DATABASES = {
+#   'default': dj_database_url.config(default=os.environ["DATABASE_URL"])
+# }
 
 
 # Password validation
